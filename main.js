2e9c01f9c7b37c35f2d0ba93d74ed8729c0b6d37
MITANSH12 <78300422+MITANSH12@users.noreply.github.com>
2022-11-29 15:47:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 function preload() {
-
+harryPotter = loadSound( music.mp3);
+peterPan = loadSound( music2.mp3);
 }
 function setup () {
 canvas = createCanvas(300 , 300);
 canvas.center();
+video = createCapture(VIDEO);
+video.hide();
 
 }
 function draw() {
 background("gainsboro");
+image (video , 0 , 0 , 300 , 300);
 }
