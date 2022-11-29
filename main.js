9c4e153e710f45f492506624010e95bbef7c538e
MITANSH12 <78300422+MITANSH12@users.noreply.github.com>
2022-11-29 16:11:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 function preload() {
-harryPotter = loadSound( music.mp3);
-peterPan = loadSound( music2.mp3);
+harryPotter = loadSound( 'music.mp3');
+peterPan = loadSound( 'music2.mp3');
 }
 function setup () {
 canvas = createCanvas(300 , 300);
 canvas.center();
 video = createCapture(VIDEO);
 video.hide();
+poseNet = ml5.posenet(video , modelLoaded)
 
 }
 function draw() {
 background("gainsboro");
 image (video , 0 , 0 , 300 , 300);
+}
+function modelLoaded () {
+    console.log('poseNet is initialized');
 }
