583e1e46dac1133081eca3a65b33294dd4391e44
MITANSH12 <78300422+MITANSH12@users.noreply.github.com>
2022-11-30 19:02:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = "";
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
 function preload() {
 harryPotter = loadSound( 'music.mp3');
 peterPan = loadSound( 'music2.mp3');
@@ -7,8 +11,8 @@ canvas = createCanvas(300 , 300);
 canvas.center();
 video = createCapture(VIDEO);
 video.hide();
-poseNet = ml5.posenet(video , modelLoaded)
-
+poseNet = ml5.poseNet(video , modelLoaded);
+poseNet.on('pose' , gotPoses);
 }
 function draw() {
 background("gainsboro");
@@ -16,4 +20,15 @@ image (video , 0 , 0 , 300 , 300);
 }
 function modelLoaded () {
     console.log('poseNet is initialized');
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+console.log(results);
+leftWristX = results[0].pose.leftWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+rightWristX = results[0].pose.rightWrist.x;
+rightWristY = results[0].pose.rightWrist.y;
+console.log("left Wrist X:"  + leftWristX , "left Wrist Y" + leftWristY);
+console.log("right Wrist X:"  + rightWristX , "right Wrist Y" + rightWristY);
+    }
 }
